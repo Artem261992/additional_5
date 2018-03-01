538adfa5b57bde32b589c6f458bf28a51ffe9c5e
Artem Nedolugov <art_nedolugov@mail.ri>
2018-03-01 17:55:37 +0300

    solv

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var a = {};
+    var cl = [];
+    var op = [];
+    var count = 0;
+
+
+    for (var i in bracketsConfig){
+        a[bracketsConfig[i][0]] = +i + 1;
+        a[bracketsConfig[i][1]] = (0 -(+i + 1));
+        cl.push(bracketsConfig[i][1]);
+        op.push(bracketsConfig[i][0]);
+    }
+
+    for (var i in str){
+        count = count + a[str[i]];
+        if (count < 0){
+            break;
+        }
+    }
+
+    if (count == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
